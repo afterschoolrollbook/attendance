30e5966843e9fc1c70e6567dfdffdef1e92b1db9
afterschoolrollbook <afterschool.rollbook@gmail.com>
2026-06-15 02:48:29 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,9 @@ import { Supplies }     from './pages/Supplies.jsx'
 import { MessageGuide } from './pages/MessageGuide.jsx'
 import { Blog }         from './pages/Blog.jsx'
 import { BlogAdmin }    from './pages/BlogAdmin.jsx'
-import { BlogMenuManage } from './pages/BlogMenuManage.jsx'
+import { BlogMenuManage }          from './pages/BlogMenuManage.jsx'
+import { RegionManage }             from './pages/RegionManage.jsx'
+import { TeacherServiceManage }     from './pages/TeacherServiceManage.jsx'
 import { BlogWrite }    from './pages/BlogWrite.jsx'
 import { ParentInvite } from './pages/ParentInvite.jsx'
 import { ParentLogin }  from './pages/ParentLogin.jsx'
@@ -488,7 +490,9 @@ function AppInner() {
       case 'supplies':        return <Supplies     user={user} />
       case 'messageguide':    return <MessageGuide user={user} />
       case 'blog_admin':      return can(user, 'manage_ad') ? <BlogAdmin user={user} /> : <Dashboard {...pageProps} />
-      case 'blog_menu_manage': return can(user, 'manage_ad') ? <BlogMenuManage user={user} /> : <Dashboard {...pageProps} />
+      case 'blog_menu_manage':        return can(user, 'manage_ad') ? <BlogMenuManage user={user} />        : <Dashboard {...pageProps} />
+      case 'region_manage':          return can(user, 'manage_ad') ? <RegionManage user={user} />          : <Dashboard {...pageProps} />
+      case 'teacher_service_manage': return can(user, 'manage_ad') ? <TeacherServiceManage user={user} /> : <Dashboard {...pageProps} />
       case 'blog_write':      return <BlogWrite user={user} onLogout={handleLogout} />
       // ✅ 본사 업체 관리 (Lv.10 전용)
       case 'vendor_manage':   return can(user, 'manage_ad') ? <VendorManage user={user} /> : <Dashboard {...pageProps} />
